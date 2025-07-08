535d8d1089d579894bc47a5ead34138cabfcf92e
nmo20 <nmo212182@gmail.com>
2025-07-09 00:36:09 +0530

    Chip  widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
   @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final List<String> filters = const [
+    'All',
+    'Adidas',
+    'Nike',
+    'Puma',
+    'Reebok',
+  ];
+
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromRGBO(255, 255, 255, 1)),
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(20)),
+    );
+
+    return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
@@ -18,13 +44,56 @@ class Homepage extends StatelessWidget {
                     style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Expanded(child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search',
-                    prefixIcon: Icon(Icons.search),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,
+                    ),
                   ),
-                ))
+                ),
               ],
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                itemCount: filters.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final label = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: GestureDetector(
+                     onTap: () {
+                        setState(() {
+                          selectedFilter = label;
+                        });
+                      },
+
+                      child: Chip(
+                        backgroundColor: selectedFilter == label
+                            ? Theme.of(context).colorScheme.primary
+                            : Color.fromRGBO(245, 247, 249, 1),
+                        side: BorderSide(
+                          color: Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                        label: Text(label),
+                        labelStyle: TextStyle(fontSize: 16),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 15,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,15 +16,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         textTheme: GoogleFonts.latoTextTheme(),
         inputDecorationTheme: InputDecorationTheme(
-          hintStyle:TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          )
+          hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
         ),
-        // colorScheme: ColorScheme(brightness: brightness, primary: primary, onPrimary: onPrimary, secondary: secondary, onSecondary: onSecondary, error: error, onError: onError, surface: surface, onSurface: onSurface)
-        // colorScheme: ColorScheme.dark(),
         colorScheme: ColorScheme.fromSeed(
           seedColor: Color.fromRGBO(254, 206, 1, 1),
+          primary: Color.fromRGBO(254, 206, 1, 1),
         ),
       ),
       home: const Homepage(),
