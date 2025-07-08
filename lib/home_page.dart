7e24ade21705bd62136085cb5f8fa3aefebd346e
nmo20 <nmo212182@gmail.com>
2025-07-08 23:42:40 +0530

    SafeArea

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,26 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+              'Shoe\nCollection',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
